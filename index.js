08d56f078d02ce5d38ff2ea9f3c408bc75baadf3
dimas oppo <dimasoppo448@gmail.com>
2022-12-03 21:21:44 +0700

    back live

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ app.sync();
 app.set("view engine", "html");
 app.set("views", path.resolve(dir, "./app/Views"));
 app.static("/assets", path.resolve(dir, "./public"));
-app.get("/", (req, res) => res.send("hwllos"));
-// app.use(...new DefaultRouter().handler);
+app.use(...new DefaultRouter().handler);
 app.notFound((req, res) => {
   res.status(404).send("404 kali");
 });
